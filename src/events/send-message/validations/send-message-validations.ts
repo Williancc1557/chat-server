3cf219f5283a5762fdfca7113a65e1660b0352ef
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-02-06 19:12:35 -0300

    Max characteres in mensage was updated

--- src/events/send-message/validations/send-message-validations.ts
+++ src/events/send-message/validations/send-message-validations.ts
@@ -2,9 +2,12 @@ export const sendMessageValidation = (author: string, message: string) => {
     const authorLenght = author.length;
     const messageLenght = message.length;
 
+    const maxLenghtMessage = 300;
+    const maxLenghtAuthor = 30;
+
     const invalidLenght = 0;
     // eslint-disable-next-line
-    const validateLenght = (messageLenght == invalidLenght || authorLenght == invalidLenght || messageLenght > 200 || author.length > 30);
+    const validateLenght = (messageLenght == invalidLenght || authorLenght == invalidLenght || messageLenght > maxLenghtMessage || author.length > maxLenghtAuthor);
     if (validateLenght) return false;
     return true;
 };
