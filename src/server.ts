44e0625985d34a67bb0a03748f3da3cb6724e49c
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-02-07 01:59:14 -0300

    bug fixed

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,10 @@ import { Server } from "socket.io";
 import { sendMessage } from "./events/send-message/send-message.event";
 import { previusMessage } from "./events/previus-message/previus-message.event";
 import { server } from "./app";
-import type { SleepDto } from "./dto/sleep.dto";
 
 const io = new Server(server);
 
 export const dataChat: Array<object> = [];
-
-export const sleepUsersData: Array<SleepDto> = [];
 
 io.on("connection", (socket: Socket) => {
     previusMessage("previusMessage", socket, dataChat);
