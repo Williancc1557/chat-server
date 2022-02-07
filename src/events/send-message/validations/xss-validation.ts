cc74c788cf638e61900ed72165cb96d873c785a3
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-02-07 00:58:58 -0300

    xss config was been updated

--- src/events/send-message/validations/xss-validation.ts
+++ src/events/send-message/validations/xss-validation.ts
@@ -3,7 +3,20 @@ import type { MessageDto } from "../../../dto/message.dto";
 import type { XssValidationDto } from "../../../dto/xss-validation.dto";
 
 export const xssValidation = (data: MessageDto): XssValidationDto => {
-    const xssValidationMessage = xss(data.message);
+    const xssValidationMessage = xss(data.message, {
+        whiteList: {
+            video: [
+                "controls",
+                "crossorigin",
+                "muted",
+                "playsinline",
+                "poster",
+                "src",
+                "height",
+                "width",
+            ],
+        },
+    });
     const xssValidationAuthor = xss(data.author);
 
     return {
